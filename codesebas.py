56526b29315191ad2297ca24f1db5476ca02cf91
Sebastián Barrón García <bgsebastian245@gmail.com>
2026-04-19 14:27:34 -0600

    Se agregaron los atributos

--- codesebas.py
+++ codesebas.py
@@ -1,2 +1,7 @@
 class Telefono:
-    pass
+    def __init__ (self, __id, __marca, __modelo, __almacenamiento, __precio):
+        self.__id = __id
+        self.__marca = __marca 
+        self.__modelo = __modelo 
+        self.__almacenamiento = __almacenamiento 
+        self.__precio = __precio
