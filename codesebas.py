d2050f64d853a513c0e145c1bd61cae188b02d73
Sebastián Barrón García <bgsebastian245@gmail.com>
2026-04-19 14:40:45 -0600

    Se agregaron getters

--- codesebas.py
+++ codesebas.py
@@ -6,3 +6,13 @@ class Telefono:
         self.__almacenamiento = almacenamiento 
         self.__precio = precio
 
+    def get_id(self):
+        return self.__id
+    def get_marca(self):
+        return self.__marca
+    def get_modelo(self):
+        return self.__modelo
+    def get_modelo(self):
+        return self.__almacenamiento
+    def get_precio(self):
+        return self.__precio
